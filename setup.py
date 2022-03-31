46c7e491322cf2e856b10a4b62242d26760da780
Vineet Bansal <vineetbansal@protonmail.com>
2022-03-31 16:37:11 -0400

    bumped version to post2

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ def readme():
 
 
 setup(name='qdldl',
-      version='0.1.5.post0',
+      version='0.1.5.post2',
       author='Bartolomeo Stellato, Paul Goulart, Goran Banjac',
       author_email='bartolomeo.stellato@gmail.com',
       description='QDLDL, a free LDL factorization routine.',
